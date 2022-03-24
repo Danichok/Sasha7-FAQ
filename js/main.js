59c9734823c18e5a2092d1f7c707b7d946edb07f
Danichok <99190797+Danichok@users.noreply.github.com>
2022-03-24 20:18:57 +0200

    FAQ

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $('.wrapper').click(function() {
     $(this).toggleClass('spoiler_open');
+    $(this).find('.features_link_arrow').toggleClass('arrow_rotate')
 });
+$('#features').click(function() {
+    $('.member_window').addClass('member_window_close');
+    $('.features_window').toggleClass('features_window_close');
+});
+$('#member').click(function() {
+    $('.features_window').addClass('features_window_close')
+    $('.member_window').toggleClass('member_window_close');
+});
+
+if ($(window).width() < 630) {
+    $('#member').attr("href", "membership.html");
+
+}
+
+if ($(window).width() < 980) {
+    $('#member').attr("href", "membership.html");
+    $('#features').attr("href", "features.html");
+}
